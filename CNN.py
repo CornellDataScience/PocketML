86116e42442bbed104af63f8557f67bbef3bbb5a
Skai Nzeuton <skainzeuton@dhcp-vl2041-5843.eduroam.cornell.edu>
2024-03-17 16:16:16 -0400

    removed wandb calls

---
+++ CNN.py
@@ -0,0 +1,145 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+# import wandb
+import random
+import numpy as np
+import os
+
+class MNIST(nn.Module):
+    # Load Data
+
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+        
+    train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+
+    test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2)
+        self.fc1 = nn.Linear(in_features=64*7*7, out_features=1024)
+        self.fc2 = nn.Linear(in_features=1024, out_features=10)
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = x.view(-1, 64*7*7)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+    
+class CIFAR(nn.Module):
+    # Load Data
+
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        
+    train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+
+    test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2)
+        self.fc1 = nn.Linear(in_features=64*8*8, out_features=1024)
+        self.fc2 = nn.Linear(in_features=1024, out_features=10)
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = x.view(-1, 64*8*8)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+def seed_everything(seed: int):
+    random.seed(seed)
+    os.environ['PYTHONHASHseed'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+# Initialize seed
+
+seed = 123
+
+seed_everything(seed)
+
+'''
+# Start a new wandb run to track this script
+wandb.init(
+    # Set the wandb project where this run will be logged
+    project="my-awesome-project",
+    
+    # Track hyperparameters and run metadata
+    config={
+    "learning_rate": 0.001,
+    "architecture": "CNN",
+    "dataset": "CIFAR-10",
+    "epochs": 1,
+    "seed": seed,
+    }
+)
+'''
+
+# Instantiate Model, Loss Function, and Optimizer
+
+model = CIFAR()
+
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Train Model
+
+for epoch in range(1):
+    running_loss = 0.0
+    for i, data in enumerate(model.train_loader, 0):
+        inputs, labels = data
+
+        optimizer.zero_grad()
+
+        outputs = model(inputs)
+        loss = loss_function(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        
+        # log metrics to wandb
+        # wandb.log({"loss": loss})
+
+        if i % 200 == 199:
+            print(f'[{epoch + 1}, {i + 1}]: loss: {running_loss / 200}')
+            running_loss = 0.0
+
+# Test Model
+            
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for data in model.test_loader:
+        images, labels = data
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+        # wandb.log({"acc": correct/total})
+
+print(f'Accuracy: {(correct/total) * 100}%')
+
+# wandb.finish()
