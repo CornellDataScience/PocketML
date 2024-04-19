4d68c60909063a912b604dd1dfc82ff37218a11d
eschneyer <emschneyer@gmail.com>
2024-04-19 17:38:52 -0400

    firebase setup added

--- PocketML_iOS/PocketML_iOS/TunerMLApp.swift
+++ PocketML_iOS/PocketML_iOS/TunerMLApp.swift
@@ -4,14 +4,26 @@
 //
 //  Created by Anya Yerramilli on 3/22/24.
 //
-
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct TunerMLApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
