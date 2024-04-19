360f25017a3df08fa0ef7612d76920082de6d83a
Michael Ngo <mn542@cornell.edu>
2024-04-19 17:51:57 -0400

    add dummy get job endpoint

---
+++ PocketML-backend/app/routers/jobs.py
@@ -0,0 +1,13 @@
+import json
+from fastapi import APIRouter
+from fastapi.responses import FileResponse
+
+router = APIRouter()
+
+
+@router.get("/api/v1/jobs")
+async def get_jobs():
+    # TODO: HOW TO SPECIFY USER AND AUTHENTICATE?
+    with open('PocketML-backend/test/sample_jobs/jobs1.json', "r") as json_file:
+        json_data = json.load(json_file)
+    return FileResponse(json_data, media_type="application/json")
