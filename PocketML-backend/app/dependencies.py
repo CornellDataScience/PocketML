322b5163c810da87504f45fb1e6d15f5679952a0
Daniel Wang <zw669@cornell.edu>
2024-04-23 15:56:48 -0400

    Add session dependency

--- PocketML-backend/app/dependencies.py
+++ PocketML-backend/app/dependencies.py
@@ -1,4 +1,16 @@
-from fastapi import HTTPException
+from typing import Annotated
+
+from fastapi import HTTPException, Depends
+from sqlmodel import Session
+from database import engine
+
+
+def get_db_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDependency = Annotated[Session, Depends(get_db_session)]
 
 
 def user_authenticated(firebase_user):
