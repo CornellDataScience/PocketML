2f2844c7a40daddacf4203bb1f179d563830be7b
Daniel Wang <zw669@cornell.edu>
2024-04-24 02:47:57 -0400

    Add firebase sdk

--- PocketML-backend/app/dependencies.py
+++ PocketML-backend/app/dependencies.py
@@ -1,8 +1,13 @@
 from typing import Annotated
 
-from fastapi import HTTPException, Depends
+from fastapi import HTTPException, Depends, Request, status
+from fastapi.security import OAuth2PasswordBearer
 from sqlmodel import Session
 from database import engine
+from config import settings
+
+import firebase_admin
+from firebase_admin import credentials, auth
 
 
 def get_db_session():
@@ -11,6 +16,12 @@ def get_db_session():
 
 
 SessionDependency = Annotated[Session, Depends(get_db_session)]
+
+
+def get_current_user(request: Request):
+    token = request.headers.get("Authorization")
+    if not token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Can not find the authorization token.")
 
 
 def user_authenticated(firebase_user):
