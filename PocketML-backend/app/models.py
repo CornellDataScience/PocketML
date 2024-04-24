0a7187c6fe5b71fa9e1b9aacbe694d5518a5ac07
Daniel Wang <zw669@cornell.edu>
2024-04-24 17:30:01 -0400

    Remove the utils.py and remove the password field from the models

--- PocketML-backend/app/models.py
+++ PocketML-backend/app/models.py
@@ -4,8 +4,7 @@ from sqlmodel import Field, Relationship, SQLModel
 class User(SQLModel, table=True):
     email: str = Field(default="un@defin.ed", primary_key=True, unique=True, index=True)
     name: str = Field(default="undefined")
-    password: str = Field(default="undefined")
-    token: str = Field(default="undefined")
+    token: str = Field(nullable=False)
     email_notif: bool = Field(default=True)
 
     is_authenticated: bool = Field(default=False)
--- PocketML-backend/app/utils.py
+++
@@ -1,8 +0,0 @@
-from fastapi import HTTPException
-
-
-def failure_response(data, code):
-    return HTTPException(
-        status_code=code,
-        detail=data,
-    )
