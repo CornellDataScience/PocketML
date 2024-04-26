2948830b0e9406dcc249089b1122f7bfc6dfecd6
everettlee <leeeverettg@gmail.com>
2024-04-26 17:28:28 -0400

    Updated models.py to add Job and CreateJob

--- PocketML-backend/app/models.py
+++ PocketML-backend/app/models.py
@@ -16,3 +16,20 @@ class CreateUser(SQLModel):
     name: str
     password: str
     email_notif: bool
+
+class Job(SQLModel, table=True):
+    name: str = Field(default="undefined", primary_key=True, unique=True, index=True)
+    wandb: bool = Field(default=False)
+    wandb_link: str = Field(default="undefined")
+    start_time: str = Field(default="undefined")
+
+    current_step: int = Field(default=0)
+    current_status: str = Field(default="stopped") # should this be undefined?
+    last_update_time: str = Field(default="undefined")
+
+    
+class CreateJob(SQLModel):
+    name: str
+    wandb: bool
+    wandb_link: str
+    start_time: str
