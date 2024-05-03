b8792e757ffb755eee74be49e560bdbc65a7c3bf
anya-yerramilli <anya.yerramilli@gmail.com>
2024-05-03 15:59:14 -0400

    Added ViewModels for API routing

--- PocketML-backend/app/routers/jobs.py
+++ PocketML-backend/app/routers/jobs.py
@@ -1,6 +1,6 @@
 import json
 from fastapi import APIRouter
-from fastapi.responses import FileResponse
+from fastapi.responses import JSONResponse
 
 
 router = APIRouter()
@@ -9,6 +9,14 @@ router = APIRouter()
 @router.get("/api/v1/jobs")
 async def get_jobs():
     # TODO: HOW TO SPECIFY USER AND AUTHENTICATE?
-    with open("PocketML-backend/test/sample_jobs/jobs1.json", "r") as json_file:
+    # /Users/anyayerramilli/CDS/PocketML-full-stack/PocketML-backend/app/test/sample_jobs/jobs1.json
+    # "/../test/sample_jobs/jobs1.json"
+
+    with open(
+        "/Users/anyayerramilli/CDS/PocketML-full-stack/PocketML-backend/app/test/sample_jobs/jobs1.json",
+        "r",
+    ) as json_file:
+        print(json_file)
         json_data = json.load(json_file)
-    return FileResponse(json_data, media_type="application/json", status_code=200)
+        print("JSON DATA", json_data)
+    return JSONResponse(json_data, status_code=200)
