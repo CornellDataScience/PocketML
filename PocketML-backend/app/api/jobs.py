fe7859bdbfbdad7eb30cb06c4c71ba89b69801f4
Daniel Wang <zw669@cornell.edu>
2024-05-03 17:22:12 -0400

    jobs update

--- PocketML-backend/app/api/jobs.py
+++ PocketML-backend/app/api/jobs.py
@@ -14,10 +14,17 @@ async def create_job(new_job: JobCreate, session: SessionDependency, token: dict
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Job already exists")
 
-    job = Job(**{
-        **new_job.dict(),
-        "user_email": token["email"],
-    })
+    job = Job(
+        name=new_job.name,
+        wandb=new_job.wandb,
+        wandb_link=new_job.wandb_link,
+        start_time=new_job.start_time,
+        config=dict_to_str(new_job.config),
+        cluster_name=new_job.cluster_name,
+        current_step=0,
+        current_status="stopped",
+        last_update_time="undefined"
+    )
 
     user = session.query(User).filter(User.email == token["email"]).first()
     if user is None:
@@ -58,60 +65,4 @@ async def update_job(*, session: SessionDependency, job_update: JobUpdate) -> Jo
     for key, value in job_update.model_dump(exclude_unset=True).items():
         setattr(job, key, value)
     session.commit()
-    return job
-
-
-
-def _get_job_by_name(*, session: Session, name: str) -> Job:
-    """
-    :return: the job with the given name
-    """
-    statement = select(Job).where(Job.name == name)
-    job = session.exec(statement).first()
-    return job
-
-
-def _create_job(*, session: Session, job_create: JobCreate) -> Job:
-    """
-    Precondition: The job does not exist in the database
-
-    :return: the created job
-    """
-    new_job = Job(
-        name=job_create.name,
-        wandb=job_create.wandb,
-        wandb_link=job_create.wandb_link,
-        start_time=job_create.start_time,
-        config=dict_to_str(job_create.config),
-        cluster_name=job_create.cluster_name,
-        current_step=0,
-        current_status="stopped",
-        last_update_time="undefined"
-    )
-
-    session.add(new_job)
-    session.commit()
-    session.refresh(new_job)
-
-    return new_job
-
-
-def _update_job(*, session: Session, job_update: JobUpdate) -> Job:
-    """
-    Update the job with the given name
-    """
-    job = session.exec(select(Job).where(Job.name == job_update.name)).first()
-    if not job:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="Internal error: could not find the job in the database")
-
-    for key, value in job_update.model_dump(exclude_unset=True).items():
-        if key == "config":
-            config = str_to_dict(job.config)
-            config = config.update(value)
-            setattr(job, key, dict_to_str(config))
-        else:
-            setattr(job, key, value)
-    session.commit()
-
     return job
