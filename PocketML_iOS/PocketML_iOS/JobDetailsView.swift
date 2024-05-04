199d747a15e38c93e756814fae34df5eeeea1741
anya-yerramilli <anya.yerramilli@gmail.com>
2024-05-03 20:38:51 -0400

    Added text fields for hyperparameter editing

--- PocketML_iOS/PocketML_iOS/JobDetailsView.swift
+++ PocketML_iOS/PocketML_iOS/JobDetailsView.swift
@@ -100,153 +100,147 @@ func WandbWidget(_ viewModel: JobInfoViewModel) -> some View {
     .padding(.trailing)
 }
 
-func HyperparamConfig(_ viewModel: JobInfoViewModel) -> some View {
-    var output = ""
-    let hyperparameters = viewModel.jobDetails?.hyperparameters ?? [:]
-    for (key, value) in hyperparameters {
-        output.append("\(key): \(value)\n")
+func HyperparamRow(_ key: String, _ value: String) -> some View{
+    // TODO: Add modifiers for color + font
+    HStack{
+        Text(key + ": ")
+        TextField("", text: .constant(value))
     }
+}
+
+func HyperparamConfig(_ viewModel: JobInfoViewModel) -> some View {
+    // var output = ""
+    let hyperparameters = viewModel.jobDetails?.hyperparameters ?? [:]
+    
     return VStack{
         Text("Model Parameters")
             .modifier(Title2Modifier())
             .bold()
-        /*
-        VStack(alignment: .leading){
-            Text("Model Type")
-            Text("Kernel")
-            Text("C")
-            Text("Max Iterations")
-            
+        ForEach(hyperparameters.sorted(by: { $0.key < $1.key }), id: \.key) { key, value in
+            HyperparamRow(key, value)
         }
-        */
-        VStack(alignment: .leading){
-            Text(output)
+    }
+}
+    
+    
+    // TODO: Integrate these widgets after MVP to incorporate connection between jobs/scripts/kernels
+    
+    func ScriptWidget(_ script: String) -> some View {
+        ZStack {
+            VStack{
+                Text(script)
+                    .modifier(TextModifier())
+                Text("<description>")
+                    .modifier(SubheadlineModifier())
+            }
+            .frame(width: 200, height: 100)
+            .background(RoundedRectangle(cornerRadius: 25)
+                .fill(Color.background2))
         }
+    }
+    
+    
+    func Graph(dataPoints: [CGFloat]) -> some View {
+        // have to figure out how to customize graph more
+        // adding axis labels + dealing with scale...
+        GeometryReader { geometry in
+            Path { path in
+                let xScale = geometry.size.width / CGFloat(dataPoints.count - 1)
+                let yScale = geometry.size.height / CGFloat(dataPoints.max() ?? 1)
+                
+                path.move(to: CGPoint(x: 0, y: dataPoints[0] * yScale))
+                
+                for index in 1..<dataPoints.count {
+                    let x = CGFloat(index) * xScale
+                    let y =  (dataPoints[index] * yScale)
+                    print(y)
+                    path.addLine(to: CGPoint(x: x, y: y))
+                }
+            }
+            .stroke(Color.purple, lineWidth: 3)
+            .background(Color.white)
+        }
+        .frame(width: 320, height: 200)
+    }
+    
+    func GraphWidget(data: [CGFloat]) -> some View {
+        Section() {
+            VStack{
+                Text("Training Loss")
+                    .modifier(SectionTitleModifier())
+                Graph(dataPoints: data)
+            }
+            .padding()
+        }
+        .background(RoundedRectangle(cornerRadius: 10)
+            .fill(Color.background2))
+        .scrollContentBackground(.hidden)
+    }
+    
+    func MilestoneTable() -> some View{
+        Text("<Milestone Table>")
+        // something like this, take input of milestone data
+        //    var body: some View {
+        //            Table(people) {
+        //                TableColumn("Name", value: \.name)
+        //                TableColumn("Last Name", value: \.lastName)
+        //                TableColumn("Ranking") {
+        //                    people in
+        //                    Text(String(people.ranking))
+        //                }
+        //            }
+    }
+    
+    func MilestonesWidget() -> some View {
+        Section() {
+            VStack{
+                Text("Milestones")
+                    .modifier(SectionTitleModifier())
+                MilestoneTable()
+            }
+            .frame(width: 320)
+            .padding()
+        }
+        .background(RoundedRectangle(cornerRadius: 10).fill(Color.background2))
         .padding(.leading)
         .padding(.trailing)
-        .background(RoundedRectangle(cornerRadius: 10.0)
-            .fill(Color.background2))
+        .scrollContentBackground(.hidden)
+        
     }
-}
-
-
-// TODO: Integrate these widgets after MVP to incorporate connection between jobs/scripts/kernels
-
-func ScriptWidget(_ script: String) -> some View {
-    ZStack {
-        VStack{
-            Text(script)
-                .modifier(TextModifier())
-            Text("<description>")
-                .modifier(SubheadlineModifier())
-        }
-        .frame(width: 200, height: 100)
-        .background(RoundedRectangle(cornerRadius: 25)
-            .fill(Color.background2))
-    }
-}
-
-
-func Graph(dataPoints: [CGFloat]) -> some View {
-    // have to figure out how to customize graph more
-    // adding axis labels + dealing with scale...
-    GeometryReader { geometry in
-        Path { path in
-            let xScale = geometry.size.width / CGFloat(dataPoints.count - 1)
-            let yScale = geometry.size.height / CGFloat(dataPoints.max() ?? 1)
-
-            path.move(to: CGPoint(x: 0, y: dataPoints[0] * yScale))
-
-            for index in 1..<dataPoints.count {
-                let x = CGFloat(index) * xScale
-                let y =  (dataPoints[index] * yScale)
-                print(y)
-                path.addLine(to: CGPoint(x: x, y: y))
-            }
-        }
-        .stroke(Color.purple, lineWidth: 3)
-        .background(Color.white)
-    }
-    .frame(width: 320, height: 200)
-}
-
-func GraphWidget(data: [CGFloat]) -> some View {
-    Section() {
-        VStack{
-            Text("Training Loss")
-                .modifier(SectionTitleModifier())
-            Graph(dataPoints: data)
-        }
-        .padding()
-    }
-    .background(RoundedRectangle(cornerRadius: 10)
-        .fill(Color.background2))
-    .scrollContentBackground(.hidden)
-}
-
-func MilestoneTable() -> some View{
-    Text("<Milestone Table>")
-    // something like this, take input of milestone data
-//    var body: some View {
-//            Table(people) {
-//                TableColumn("Name", value: \.name)
-//                TableColumn("Last Name", value: \.lastName)
-//                TableColumn("Ranking") {
-//                    people in
-//                    Text(String(people.ranking))
-//                }
-//            }
-}
-
-func MilestonesWidget() -> some View {
-    Section() {
-        VStack{
-            Text("Milestones")
-                .modifier(SectionTitleModifier())
-            MilestoneTable()
-        }
-        .frame(width: 320)
-        .padding()
-    }
-    .background(RoundedRectangle(cornerRadius: 10).fill(Color.background2))
-    .padding(.leading)
-    .padding(.trailing)
-    .scrollContentBackground(.hidden)
     
-}
-
-func ActionButton(action:String) -> some View{
-    ZStack{
-
-        Text(action)
-            .foregroundColor(Color.main)
-            .frame(width: 100, height: 50)
-            .background(RoundedRectangle(cornerRadius: 25)
-                .fill(Color.background))
-            .padding()
+    func ActionButton(action:String) -> some View{
+        ZStack{
+            
+            Text(action)
+                .foregroundColor(Color.main)
+                .frame(width: 100, height: 50)
+                .background(RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.background))
+                .padding()
+        }
     }
-}
-
-func ActionsWidget() -> some View{
-    Section(){
-        VStack{
-            Text("Actions")
-                .modifier(TitleModifier())
-                .padding(.bottom, -25)
-            HStack(spacing:1){
-                ActionButton(action:"Start")
-                    .padding(.leading)
-                ActionButton(action:"Stop")
-                ActionButton(action:"Pause")
-                    .padding(.trailing)
+    
+    func ActionsWidget() -> some View{
+        Section(){
+            VStack{
+                Text("Actions")
+                    .modifier(TitleModifier())
+                    .padding(.bottom, -25)
+                HStack(spacing:1){
+                    ActionButton(action:"Start")
+                        .padding(.leading)
+                    ActionButton(action:"Stop")
+                    ActionButton(action:"Pause")
+                        .padding(.trailing)
+                    
+                }
                 
             }
-
         }
+        .background(Color.background2)
     }
-    .background(Color.background2)
-}
+    
+    #Preview {
+        JobDetailsView(selectedJob: Job(jobTitle: "Job 1", completedEpochs: 1.25, totalEpochs: 5.0, jobID: "", isActive: true))
+    }
 
-#Preview {
-    JobDetailsView(selectedJob: Job(jobTitle: "Job 1", completedEpochs: 1.25, totalEpochs: 5.0, jobID: "", isActive: true))
-}
